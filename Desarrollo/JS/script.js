fdc3cc40d7ba2557af741e9326dda742b08a7c17
Pilar <pilarlugones0@gmail.com>
2026-04-03 11:46:27 -0300

    feat: crea base de generación de colores aleatorios con formato HEX

--- Desarrollo/JS/script.js
+++ Desarrollo/JS/script.js
@@ -1,1 +1,14 @@
 console.log('¡JavaScript está funcionando!');
+
+function generarColores(numColores) {
+    const contenedor = document.getElementById("contenedor-colores");
+    contenedor.innerHTML = "";
+    for (let i = 0; i < numColores; i++) {
+        const coloresRandom = generarHEX();
+        const colorBox = document.createElement("div");
+        colorBox.className = "color-box";
+        colorBox.texto = coloresRandom;
+        contenedor.appendChild(colorBox);}
+        return generarColores
+    };
+
