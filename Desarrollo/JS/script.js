0a4dba6ef59450ed827e44ebb3c9e2856bc1a5ef
Pilar <pilarlugones0@gmail.com>
2026-04-05 13:20:44 -0300

    feat: se declara funcion matematica para convertir formato HSL a HEX, relacionandolo a la función de mostrarPaleta donde se usa if y else dependiendo en que boton de formato haga click el usuario

--- Desarrollo/JS/script.js
+++ Desarrollo/JS/script.js
@@ -86,3 +86,25 @@ function mostrarPaleta () {
         contenedorColores.appendChild(colorBox);
     }
 }
+
+btnGenerar.addEventListener('click', function() {
+    paletaColores = [];
+    for (let i = 0; i < tamañoPaleta; i++) {
+        paletaColores.push(generarHSLcolor());
+    }
+    mostrarPaleta();
+});
+
+function convertirHSLaHEX (hsl) {
+    const hslValues = hsl.match(/\d+/g).map(Number);
+    const h = hslValues[0];
+    const s = hslValues[1] / 100;
+    const l = hslValues[2] / 100;
+    const a = s * Math.min(l, 1 - l);
+    const f = n => {
+        const k = (n + h / 30) % 12;
+        const color = l - a * Math.max(Math.min(k - 3, 9 - k, 1), -1);
+        return Math.round(255 * color).toString(16).padStart(2, '0');
+    };
+    return `#${f(0)}${f(8)}${f(4)}`;
+}
